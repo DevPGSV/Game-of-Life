d195180dd3288b8f5db8bcc31669d96252f5b0eb
DevPGSV <DevPGSV@gmail.com>
2015-10-14 10:39:59 +0200

    Basic Surface class

---
+++ esRuedaPGSV/GameOfLife/Surface.java
@@ -0,0 +1,27 @@
+package esRuedaPGSV.GameOfLife;
+
+public class Surface {
+	private Cell[][] surface;
+	private int rows;
+	private int columns;
+	
+	public Surface(int numRows, int numColumns){
+		this.rows = numRows;
+		this.columns = numColumns;
+		
+		// Coords c = new Coords(1, 2);
+		
+		/*Cell cell = new Cell(1,3);
+		boolean dead = cell.loseLp();
+		if (cell.loseLp()) {
+			
+		}
+		
+		System.out.print(cell);
+		
+		*/
+	}
+	
+
+}
+
