d841527ad36009713d7988f27b1e5b9a923e9e62
DevPGSV <DevPGSV@gmail.com>
2015-10-14 10:39:28 +0200

    Basic Controller class

---
+++ esRuedaPGSV/GameOfLife/Controller.java
@@ -0,0 +1,17 @@
+package esRuedaPGSV.GameOfLife;
+
+import java.util.Scanner;
+
+public class Controller {
+	private World world;
+	private Scanner in;
+	
+	public Controller(World world, Scanner in){
+		this.world = world;
+		this.in = in;
+		
+	}
+	public void executeSimulation(){
+		
+	}
+}
