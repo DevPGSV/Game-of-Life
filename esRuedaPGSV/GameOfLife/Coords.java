56434ab636530fc4db80e796d885412c38b5bc4c
DevPGSV <DevPGSV@gmail.com>
2015-10-14 10:39:38 +0200

    Coords class

---
+++ esRuedaPGSV/GameOfLife/Coords.java
@@ -0,0 +1,29 @@
+package esRuedaPGSV.GameOfLife;
+
+public class Coords {
+	private int row;
+	private int column;
+	
+	public Coords(int row, int column){
+		this.row = row;
+		this.column = column;
+	}
+	
+	public int getRow(){
+		return row;
+	}
+	
+	public int getColumn(){
+		return column;
+	}
+	
+	public void setRow(int row){
+		this.row = row;
+		
+	}
+	
+	public void setColumn(int column){
+		this.column = column;
+	}
+	
+}
