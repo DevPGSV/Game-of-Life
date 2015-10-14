fa12d5d27e4d6b29b2b7daabfe1576a0ffcdeb40
DevPGSV <DevPGSV@gmail.com>
2015-10-14 11:12:56 +0200

    Added documentation to World class

--- esRuedaPGSV/GameOfLife/World.java
+++ esRuedaPGSV/GameOfLife/World.java
@@ -1,13 +1,38 @@
 package esRuedaPGSV.GameOfLife;
 
 public class World {
-
 	private Surface surface;
+	
 	public World(){
 		
 	}
+	
+	/**
+	 * Execute a simulation step
+	 *
+	 */
 	public void evolve(){
 		
+	}
+	
+	/**
+	 * Creates a new cell at the specified coordinates
+	 * 
+	 * @param coords coordinates
+	 * @return      if it was possible to create the cell at the given coordinates
+	 */
+	public boolean createCell(Coords coords) {
+		return false;
+	}
+	
+	/**
+	 * Creates a new cell at the specified coordinates
+	 * 
+	 * @param coords coordinates
+	 * @return      if it was possible to delete the cell at the given coordinates
+	 */
+	public boolean deleteCell(Coords coords) {
+		return false;
 	}
 	
 }
