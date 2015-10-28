50e69ce7383f64940f4a1ce497db626447e759f4
DevPGSV <DevPGSV@gmail.com>
2015-10-28 15:12:18 +0100

    Surface creates cells at random positions, gets hardcoded data from Values class, and correctly moves cells each iteration

--- esRuedaPGSV/GameOfLife/Surface.java
+++ esRuedaPGSV/GameOfLife/Surface.java
@@ -13,14 +13,18 @@ public class Surface {
 		this.rows = numRows;
 		this.columns = numColumns;
 		
+		Random rand = new Random();
+		
 		surface = new Cell[rows][columns];
 		
 		for (int i = 0; i < rows; i++){
 			for (int j = 0; j < columns; j++){
-				surface[i][j] = new Cell(1, 2);
+				if ((rand.nextInt() % 2) == 0) {
+					surface[i][j] = new Cell(Values.MAX_LP, Values.MAX_MP);
+				}
+				
 			}
 		}
-		
 		
 		// Coords c = new Coords(1, 2);
 		
@@ -43,7 +47,7 @@ public class Surface {
 	 */
 	public boolean createCell(Coords coords) {
 		if (surface[coords.getRow()][coords.getColumn()] == null) {
-			surface[coords.getRow()][coords.getColumn()] = new Cell(1, 2);
+			surface[coords.getRow()][coords.getColumn()] = new Cell(Values.MAX_LP, Values.MAX_MP);
 			return true;
 		} else {
 			return false;
@@ -74,23 +78,25 @@ public class Surface {
 	public boolean moveCell(Coords coords){
 		Coords tempCoords = new Coords(0,0);
 		List<Coords> freeSpots = new ArrayList<Coords>();
-		
+		//System.out.println("=> " + coords);
 		for (int i = -1; i <= 1; i++){
 			for (int j = -1; j <= 1; j++){
 				tempCoords.setRow(coords.getRow() + i);
 				tempCoords.setColumn(coords.getColumn() + j);
-				if(tempCoords.getRow() != coords.getRow() && (tempCoords.getColumn() != coords.getColumn())){
+				if(tempCoords.getRow() != coords.getRow() || (tempCoords.getColumn() != coords.getColumn())){
 					if((tempCoords.getRow() >= 0) && (tempCoords.getRow() < this.rows)) {
 						if((tempCoords.getColumn() >= 0) && (tempCoords.getColumn() < this.columns))
 						{
 							if (surface[tempCoords.getRow()][tempCoords.getColumn()] == null) {
-								freeSpots.add(tempCoords); // Add available position to freeSpots list
+								freeSpots.add(new Coords(tempCoords)); // Add available position to freeSpots list
 							}
 						}
 					}
 				}
 			}
 		}
+		//System.out.println(freeSpots.size());
+		//System.out.println(freeSpots);
 		
 		if (freeSpots.isEmpty()) { // If no available positions
 			surface[coords.getRow()][coords.getColumn()].loseLp();
@@ -103,6 +109,7 @@ public class Surface {
 			surface[choosenCoords.getRow()][choosenCoords.getColumn()].maturate();
 			return true;
 		}
+		
 	}
 	
 	/**
@@ -112,18 +119,35 @@ public class Surface {
 		for (int i = 0; i < rows; i++){
 			for (int j = 0; j < columns; j++){
 				if (surface[i][j] != null) {
-					moveCell(new Coords(i, j));
+					surface[i][j].setMovable(true);
 				}
 			}
 		}
+		for (int i = 0; i < rows; i++){
+			for (int j = 0; j < columns; j++){
+				if (surface[i][j] != null) {
+					if (surface[i][j].isMovable()) {
+						surface[i][j].setMovable(false);
+						moveCell(new Coords(i, j));
+					}
+				}
+			}
+		}
+		
+		
 	}
 
 	public String toString() {
 		String board = "";
+		board = board + "|   0   |   1   |   2   |   3   |   4   |\n";
 		for (int i = 0; i < rows; i++){
 			board = board +  "|  ";
 			for (int j = 0; j < columns; j++){
-				board = board + surface[i][j] + "  |  ";
+				if (surface[i][j] != null) {
+					board = board + surface[i][j] + "  |  ";
+				} else {
+					board = board + "~~~" + "  |  ";
+				}
 			}
 			board = board + "\n"; // System.out.println("\n");
 		}
