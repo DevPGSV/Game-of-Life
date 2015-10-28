7433569d6e6ef86702c0afc5d1e27fd0d6083184
DevPGSV <DevPGSV@gmail.com>
2015-10-28 15:08:11 +0100

    Added Values class (for hardcoded values)

---
+++ esRuedaPGSV/GameOfLife/Values.java
@@ -0,0 +1,10 @@
+package esRuedaPGSV.GameOfLife;
+
+public class Values {
+	public static final int MAX_LP = 3;
+	public static final int MAX_MP = 3;
+	
+	public static final int BOARD_ROWS = 4;
+	public static final int BOARD_COLS = 4;
+	
+}
