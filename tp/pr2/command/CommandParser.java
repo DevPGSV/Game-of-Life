bac3b56465842e26ec995194ef52a6863b1989e2
DevPGSV <DevPGSV@gmail.com>
2015-12-08 20:21:38 +0100

    Added CommandParser class

---
+++ tp/pr2/command/CommandParser.java
@@ -0,0 +1,27 @@
+package tp.pr2.command;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CommandParser {
+	
+	private static List<Command> availableCommands = Arrays.asList(new HelpCommand());
+	
+	public static String helpTextCommands() {
+		StringBuilder helpText = new StringBuilder();
+		helpText.append("AVAILABLE COMMANDS: \n");
+		for(Command command : availableCommands) {
+			helpText.append("   {BOLD}" +  command.toString().toUpperCase() + "{RESET}  " + command.helpText() + ".\n");
+		}
+		return helpText.toString();
+	}
+	
+	public static Command parseCommand(String[] commandString) {
+		Command cObject;
+		for(Command command : availableCommands) {
+			cObject = command.parse(commandString);
+			if (cObject != null) return cObject;
+		}
+		return null;
+	}
+}
