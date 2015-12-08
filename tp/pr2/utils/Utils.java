21b0cb1169b39647679702271007659c07230dd5
DevPGSV <DevPGSV@gmail.com>
2015-12-08 18:17:23 +0100

    Added "checkIfOccupied" parameter to getAvailablePositions and getRandomAvailablePosition

--- tp/pr2/utils/Utils.java
+++ tp/pr2/utils/Utils.java
@@ -16,7 +16,7 @@ public class Utils {
 	 * @param surface surface instance
 	 * @return      a coordinate list with positions around the specified coordinates
 	 */
-	public static List<Coords> getAvailablePositions(Coords coords, Surface surface){
+	public static List<Coords> getAvailablePositions(Coords coords, Surface surface, boolean checkIfOccupied){
 		Coords tempCoords = new Coords(0,0);
 		List<Coords> positionsAround = new ArrayList<Coords>();
 		//System.out.println("=> " + coords);
@@ -24,14 +24,11 @@ public class Utils {
 			for (int j = -1; j <= 1; j++){
 				tempCoords.setRow(coords.getRow() + i);
 				tempCoords.setColumn(coords.getColumn() + j);
-				if(tempCoords.getRow() != coords.getRow() || (tempCoords.getColumn() != coords.getColumn())){
-					if((tempCoords.getRow() >= 0) && (tempCoords.getRow() < surface.getRows())) {
+				if(tempCoords.getRow() != coords.getRow() || (tempCoords.getColumn() != coords.getColumn()))
+					if((tempCoords.getRow() >= 0) && (tempCoords.getRow() < surface.getRows())) 
 						if((tempCoords.getColumn() >= 0) && (tempCoords.getColumn() < surface.getColumns()))
-						{
-							positionsAround.add(new Coords(tempCoords)); // Add available position to freeSpots list
-						}
-					}
-				}
+							if (!checkIfOccupied || surface.isPositionEmpty(tempCoords))
+								positionsAround.add(new Coords(tempCoords)); // Add available position to freeSpots list
 			}
 		}
 		return positionsAround;
@@ -44,8 +41,8 @@ public class Utils {
 	 * @param surface
 	 * @return
 	 */
-	public static Coords getRandomAvailablePosition(Coords coords, Surface surface) {
-		List<Coords> positionsAround = Utils.getAvailablePositions(coords, surface);
+	public static Coords getRandomAvailablePosition(Coords coords, Surface surface, boolean checkIfOccupied) {
+		List<Coords> positionsAround = Utils.getAvailablePositions(coords, surface, checkIfOccupied);
 		
 		if (positionsAround.isEmpty()) { // If no available positions
 			return null;
