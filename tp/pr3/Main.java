4d3300d77b22beb63ea12fb46143f02b6b48b5de
DevPGSV <DevPGSV@gmail.com>
2016-01-17 18:41:55 +0100

    Choose Look And Feel

--- tp/pr3/Main.java
+++ tp/pr3/Main.java
@@ -2,9 +2,13 @@ package tp.pr3;
 
 import java.util.Scanner;
 
+import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
+import javax.swing.UnsupportedLookAndFeelException;
+
 import tp.pr3.controller.Controller;
-import tp.pr3.logic.SimpleWorld;
-import tp.pr3.logic.World;
+import tp.pr3.logic.world.SimpleWorld;
+import tp.pr3.logic.world.World;
 import tp.pr3.utils.SingleRandom;
 
 /**
@@ -21,15 +25,37 @@ public class Main {
 	 * @param args Arguments ignored
 	 */
 	public static void main(String[] args) {
-		long rndSeed = 54545515;
-		SingleRandom.getInstance().getRandom().setSeed(rndSeed);
+		
+		SingleRandom.getInstance().getRandom().setSeed(1);
+		
+		try {
+		    for (LookAndFeelInfo laf : UIManager.getInstalledLookAndFeels()) {
+		        if ("Nimbus".equals(laf.getName())) {
+		            UIManager.setLookAndFeel(laf.getClassName());
+		            break;
+		        }
+		    }
+		} catch (Exception e) {
+		    // If Nimbus is not available, you can set the GUI to another look and feel.
+		}
+		
+		if (!UIManager.getLookAndFeel().getName().equals("Nimbus")) {
+			try {
+				UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+				UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+			} catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 		
 		
-		World world = new SimpleWorld(4, 4);
+		
+		
+		World world = new SimpleWorld();
 		Scanner in = new Scanner(System.in);
 		Controller controller = new Controller(world, in);
 		controller.executeSimulation();
-		
 	}
 
 }
